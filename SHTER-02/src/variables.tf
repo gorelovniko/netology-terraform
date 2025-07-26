38d1939b5fc6ed5205d6073469905469c219a4b1
gorelovniko <gorelovniko@yandex.ru>
2025-07-26 14:40:04 +0500

    SHTER-02.1-6 done fix

--- SHTER-02/src/variables.tf
+++ SHTER-02/src/variables.tf
@@ -2,17 +2,10 @@
 
 variable "cloud_id" {
   type        = string
-
-  # Удалить перед git pull
-  default = "b1g51144vtfee9bo6o2e"
-
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
-
-  default = "b1gk1t0oue1nvd45alcf"
-
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
